692605a637574206502f3cc69e0b1d63c4d65ff5
Joost de Vries <joost-de-vries@users.noreply.github.com>
2021-10-05 22:01:37 +0200

    cleanup

--- client/stream.ts
+++ client/stream.ts
@@ -14,6 +14,26 @@ for await (const value of gen) {
 }
 
 // uses es2018.asyncgenerator
+
+// using ReadableStream
+async function* ndJsonStream<A>(response: Response): AsyncGenerator<A, void> {
+    const decoder = new TextDecoder()
+    const bodyStream = response.body
+    if (bodyStream === null) {
+        throw Error(`Expected ${ndJsonMimeType} response body`)
+    }
+
+    for await(const value of bodyStream) {
+        const chunk = decoder.decode(value, { stream: true }).trim()
+
+        if (chunk.length > 0) {
+            yield JSON.parse(chunk)
+        } 
+    }
+    return
+}
+
+// using ReadableStreamDefaultReader
 async function* ndJsonReader<A>(response: Response): AsyncGenerator<A, void> {
   const decoder = new TextDecoder()
   const bodyReader = response.body?.getReader()
@@ -41,20 +61,4 @@ async function* ndJsonReader<A>(response: Response): AsyncGenerator<A, void> {
   } finally {
     bodyReader.releaseLock();
   }
-}
-
-async function* ndJsonStream<A>(response: Response): AsyncGenerator<A, void> {
-    const decoder = new TextDecoder()
-    const bodyStream = response.body
-    if (bodyStream === null) {
-    throw Error(`Expected ${ndJsonMimeType} response body`)
-    }
-
-    for await( const value of bodyStream) {
-    const chunk = decoder.decode(value, { stream: true }).trim()
-
-    if (chunk.length > 0) {
-        yield JSON.parse(chunk)
-    } 
-    }
 }
